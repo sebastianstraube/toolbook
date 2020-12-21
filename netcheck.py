afae459fe730f3c38ee4551da4b7d037a3824c80
Sebastian Straube <github@straube.ch>
2020-12-21 10:37:31 +0100

    netcheck log_mode

--- netcheck.py
+++ netcheck.py
@@ -10,19 +10,23 @@ REMOTE_SERVER = "1.1.1.1"
 FILE_PATH= "/home/admin/Desktop/netcheck.log"
 FILE = open(FILE_PATH, 'w', buffering=1)
 TIMEZONE="Europe/Zurich"
+LOG_MODE=1 #1:ERROR, 2:TRACE
 
 def is_connected(hostip):
   try:
     s = socket.create_connection((hostip, 80), 100)
     s.close()
     return "pass"
-  except:
-    pass
+  except socket.error as exc:
+    if LOG_MODE >= 2:
+      print('[ERROR] socket exception ', exc + '\n')
+      FILE.write('[ERROR] socket exception ' + exc + '\n')
+
   return "nopass"
 
 def checkUntil(endTime):
-  print('check on remote ip: ' + REMOTE_SERVER + '\n')
-  FILE.write('check on remote ip: ' + REMOTE_SERVER + '\n')
+  print('check remote ip: ' + REMOTE_SERVER + '\n')
+  FILE.write('check remote ip: ' + REMOTE_SERVER + '\n')
 
   while isExitTimeReached(endTime) == False:
     time.sleep(0.25)
@@ -30,11 +34,12 @@ def checkUntil(endTime):
       
     try:
       if is_connected(REMOTE_SERVER) != "pass":
-        print('no internet', mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + '\n')
+        print('[ERROR] check ', mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + '\n')
         FILE.write('[ERROR] check ' + mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + '\n')  # python will convert \n to os.linesep
       else:
-        print('[OK] check at ' + mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + '\n')
-        FILE.write('[OK] check ' + mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + '\n')
+        if LOG_MODE >= 2:
+          print('[OK] check ' + mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + '\n')
+          FILE.write('[OK] check ' + mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + '\n')
     except:
       FILE.close()
       break
