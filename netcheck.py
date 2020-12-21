51ed16d134225e6e3e8a4fcb0cc96f1dfbd99573
Sebastian Straube <github@straube.ch>
2020-12-21 15:43:36 +0100

    logging

--- netcheck.py
+++ netcheck.py
@@ -6,55 +6,63 @@ from dateutil import parser, tz
 from datetime import datetime
 import time
 
-REMOTE_SERVER = "1.1.1.1"
-FILE_PATH= "/home/admin/Desktop/netcheck.log"
-FILE = open(FILE_PATH, 'w', buffering=1)
+#configuration
+LOG_MODE=2 #1:ERROR, 2:TRACE
+CONSOLE_LOG=True
+SLEEP_TIMER=0.25
+CONNECTION_TRY=1000
+
+#parameters
 TIMEZONE="Europe/Zurich"
-LOG_MODE=1 #1:ERROR, 2:TRACE
+CHECKUNTIL_TIME="Dec 20, 2021 0:30 AM"
+REMOTE_SERVER_DNS = "one.one.one.one"
+REMOTE_SERVER_IP = "1.1.1.1"
+REMOTE_SERVER_PORT = "80" #HTTP
+REMOTE_SERVER_RETRYTIME = 1
+FILE_PATH= "/home/admin/Desktop/netcheck.log"
 
-def is_connected(hostip):
-  try:
-    s = socket.create_connection((hostip, 80), 100)
-    s.close()
-    return "pass"
-  except socket.error as exc:
-    if LOG_MODE >= 2:
-      print('[ERROR] socket exception ', exc + '\n')
-      FILE.write('[ERROR] socket exception ' + exc + '\n')
-
-  return "nopass"
-
-def checkUntil(endTime):
-  print('check remote ip: ' + REMOTE_SERVER + '\n')
-  FILE.write('check remote ip: ' + REMOTE_SERVER + '\n')
-
-  while isExitTimeReached(endTime) == False:
-    time.sleep(0.25)
+def logging_error(msg):
+  if LOG_MODE >= 1:
     mydate=datetime.now(tz=tz.tzlocal())
-      
-    try:
-      if is_connected(REMOTE_SERVER) != "pass":
-        print('[ERROR] check ', mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + '\n')
-        FILE.write('[ERROR] check ' + mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + '\n')  # python will convert \n to os.linesep
-      else:
-        if LOG_MODE >= 2:
-          print('[OK] check ' + mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + '\n')
-          FILE.write('[OK] check ' + mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + '\n')
-    except:
-      FILE.close()
-      break
-  FILE.close()
+    FILE.write('[ERROR] '+ mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + ', ' + msg + '\n')
+    if CONSOLE_LOG == True:
+      print('[ERROR] '+ mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + ', ' + msg + '\n')
+    
+def logging_trace(msg):
+  if LOG_MODE >= 2:
+    mydate=datetime.now(tz=tz.tzlocal())
+    FILE.write('[TRACE] ' + mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + ', ' + msg + '\n')
+    if CONSOLE_LOG == True:
+      print('[TRACE] ' + mydate.strftime("%Y-%m-%d %H:%M:%S-%f") + ', ' + msg + '\n')
+
+def is_connected(hostip, port, retryTime):
+  try:
+    s = socket.create_connection((hostip, port), retryTime)
+    s.close()
+    logging_trace("socket opened, connection established and socket closed")
+  except socket.error as exc:
+    logging_error("OSError: {0}".format(exc))
+    return False
+  return True
 
 def isExitTimeReached(mydatetime):
   PYCON_DATE = parser.parse(mydatetime)
   PYCON_DATE = PYCON_DATE.replace(tzinfo=tz.gettz(TIMEZONE))
   now = datetime.now(tz=tz.tzlocal())
-
   countdown = PYCON_DATE - now
   if(countdown.total_seconds() <= 0):
-    print("end time reached, exit ...")
-    FILE.write('end time reached, exit...')
+    logging_trace("end time reached, exit ...")
     return True
   return False
 
-checkUntil("Dec 20, 2021 0:30 AM")
+def checkUntil(endTime, checkIp, checkPort, checkRetryTime):
+  logging_trace('check remote ip: ' + checkIp)
+
+  while isExitTimeReached(endTime) == False:
+      is_connected(checkIp, checkPort, checkRetryTime)
+      time.sleep(SLEEP_TIMER)
+
+#Program entrypoint
+FILE = open(FILE_PATH, 'a', buffering=1)
+checkUntil(CHECKUNTIL_TIME, REMOTE_SERVER_IP, REMOTE_SERVER_PORT, REMOTE_SERVER_RETRYTIME)
+FILE.close()
